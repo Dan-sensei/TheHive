c2a6da6ab77f343d41bc2080c7f9424e752c21e6
pablomanez <pablomanez@hotmail.com>
2019-01-30 15:06:58 +0100

    Matrix stack hecha y funcional en TTransform

--- thehive/src/SurrealEngine/TEntidad.hpp
+++ thehive/src/SurrealEngine/TEntidad.hpp
@@ -1,11 +1,22 @@
 #ifndef TENTIDAD_H
 #define TENTIDAD_H
 
+#include <stack>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+
 class TEntidad {
     public:
         virtual ~TEntidad()         = 0;
         virtual void beginDraw()    = 0;
         virtual void endDraw()      = 0;
+
+    protected:
+        static std::stack<glm::mat4> glMatrixStack;
 };
 
 #endif
