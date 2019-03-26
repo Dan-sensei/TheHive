0d22aa3b9c9c1b8408fc4e0ff8b755d96b99a6fe
pablomanez <pablomanez@hotmail.com>
2019-03-26 23:20:13 +0100

    Nave cambiada

--- thehive/src/Game.cpp
+++ thehive/src/Game.cpp
@@ -94,7 +94,7 @@ void Game::Init(){
     // BinaryParser::LoadLevelData("assets/BinaryFiles/TAMESIS.data");
     // BinaryParser::LoadLevelData("assets/BinaryFiles/END.data");
     Engine2D->InitHUD();
-    
+
 
     auto sF = Singleton<Factory>::Instance();
     Engine->crearCamara(90,0.15f,300.f, glm::vec3(2,2,10),glm::vec3(),16.f/9.f);
