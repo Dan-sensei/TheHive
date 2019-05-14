31c0141e04d56ce2021a76d7b0aa92c2bb098fc4
olareto <olareto@gmail.com>
2019-05-14 15:56:15 +0200

    merge luces

--- thehive/src/Omicron/DeferredShading.cpp
+++ thehive/src/Omicron/DeferredShading.cpp
@@ -4,6 +4,14 @@
 #include <Singleton.hpp>
 #include <iostream>
 #include <Omicron/AssetManager.hpp>
+#include <Omicron/TLuz.hpp>
+
+#define tamanyo 1024
+#define Ldirec   0
+#define Lcolor  16
+#define Lint   28
+#define Nfocales   32
+#define Npuntuales   36
 
 struct LUZF
 {
@@ -34,7 +42,74 @@ struct LUZF
   float intensidad;
 };
 
+void DeferredShading::setnluces(int nluces_F,int nluces_p){
+    *((float *)(buffer + Nfocales)) = nluces_F;
+    *((float *)(buffer + Npuntuales)) = nluces_p;
+}
+void DeferredShading::setDirLuz(glm::vec3 dir,glm::vec3 color,float inten){
+    ((float*)(buffer + Ldirec))[0] = dir.x;
+    ((float*)(buffer + Ldirec))[1] = dir.y;
+    ((float*)(buffer + Ldirec))[2] = dir.z;
 
+    ((float*)(buffer + Lcolor))[0] = color.x;
+    ((float*)(buffer + Lcolor))[1] = color.y;
+    ((float*)(buffer + Lcolor))[2] = color.z;
+
+    *((float *)(buffer + Lint)) = inten;
+}
+void DeferredShading::info(){
+
+    int tam=12;
+    static const GLchar * uniformNames[12] =
+    {
+        "light.dirluzD",
+        "light.colorluzD",
+        "light.intluzD",
+        "light.NNlucesF",
+        "light.NNlucesP",
+        "light.posluzF",
+        "light.posfocoluzF",
+        "light.colorluzF",
+        "light.intluzF",
+        "light.posluzP",
+        "light.colorluzP",
+        "light.intluzP"
+    };
+    GLuint uniformIndices[tam];
+    glGetUniformIndices(DEFERRED_SHADER->getID(), tam, uniformNames, uniformIndices);
+    GLint uniformOffsets[tam];
+    GLint arrayStrides[tam];
+    GLint matrixStrides[tam];
+    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), tam, uniformIndices,
+    GL_UNIFORM_OFFSET, uniformOffsets);
+    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), tam, uniformIndices,
+    GL_UNIFORM_ARRAY_STRIDE, arrayStrides);
+    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), tam, uniformIndices,
+    GL_UNIFORM_MATRIX_STRIDE, matrixStrides);
+
+
+    std::cout << "#define Ldirec   " <<uniformOffsets[0]<< '\n';
+    std::cout << "#define Lcolor  " <<uniformOffsets[1]<< '\n';
+    std::cout << "#define Lint   " <<uniformOffsets[2]<< '\n';
+    std::cout << "#define Nfocales   " <<uniformOffsets[3]<< '\n';
+    std::cout << "#define Npuntuales   " <<uniformOffsets[4]<< '\n';
+    std::cout << "Luz dirigida" << '\n';
+    for (size_t i = 5; i < 9; i++) {
+        std::cout << "#define U"<<i<<"    " <<uniformOffsets[i]<< '\n';
+        std::cout << "#define S"<<i<<"    " <<arrayStrides[i]<< '\n';
+    }
+
+
+    //std::cout << "final" <<offset4<< '\n';
+    std::cout << "Luz puntuales" << '\n';
+    for (size_t i = 9; i < 12; i++) {
+        std::cout << "#define U"<<i<<"    " <<uniformOffsets[i]<< '\n';
+        std::cout << "#define S"<<i<<"    " <<arrayStrides[i]<< '\n';
+    }
+
+    std::cout << "/* max */" << uniformOffsets[11]+arrayStrides[11]*9<<'\n';
+
+}
 DeferredShading::DeferredShading(){}
 
 DeferredShading::~DeferredShading(){
@@ -55,124 +130,19 @@ void DeferredShading::init(uint16_t SCREEN_WIDTH, uint16_t SCREEN_HEIGHT){
     DEFERRED_SHADER = Singleton<AssetManager>::Instance()->getShader("DEFERRED");
     POSTPROCESSING_SHADER = Singleton<AssetManager>::Instance()->getShader("PostProcessing");
     ///UBO ligth
+
     block_index=DEFERRED_SHADER->getUniformBlockLocation("light");
+    buffer = (unsigned char *)malloc(tamanyo);
+    TLuz::buffer=buffer;
 
+    setDirLuz(glm::vec3(0,1,0),glm::vec3(1,1,1),0.1);
+    setnluces(0,0);
 
-    static const GLchar * uniformNames[10] =
-    {
-        "light.dirluzD",
-        "light.colorluzD",
-        "light.intluzD",
-        "light.posluzF",
-        "light.posfocoluzF",
-        "light.colorluzF",
-        "light.intluzF",
-        "light.posluzP",
-        "light.colorluzP",
-        "light.intluzP"
-    };
-    GLuint uniformIndices[10];
-    glGetUniformIndices(DEFERRED_SHADER->getID(), 10, uniformNames, uniformIndices);
-    GLint uniformOffsets[10];
-    GLint arrayStrides[10];
-    GLint matrixStrides[10];
-    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), 10, uniformIndices,
-    GL_UNIFORM_OFFSET, uniformOffsets);
-    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), 10, uniformIndices,
-    GL_UNIFORM_ARRAY_STRIDE, arrayStrides);
-    glGetActiveUniformsiv(DEFERRED_SHADER->getID(), 10, uniformIndices,
-    GL_UNIFORM_MATRIX_STRIDE, matrixStrides);
-
-
-    buffer = (unsigned char *)malloc(8096);
-
-    //dir
-    ((float*)(buffer + uniformOffsets[0]))[0] = 0.0f;
-    ((float*)(buffer + uniformOffsets[0]))[1] = 1.0f;
-    ((float*)(buffer + uniformOffsets[0]))[2] = 0.0f;
-    //((float*)(buffer + uniformOffsets[0]))[0] = 10.0f;
-    //((float*)(buffer + uniformOffsets[0]))[1] = 4.82238f;
-    //((float*)(buffer + uniformOffsets[0]))[2] = -39.5693f;
-    //color
-    ((float*)(buffer + uniformOffsets[1]))[0] = 1.0f;
-    ((float*)(buffer + uniformOffsets[1]))[1] = 1.0f;
-    ((float*)(buffer + uniformOffsets[1]))[2] = 1.0f;
-
-    //ints dire
-    *((float *)(buffer + uniformOffsets[2])) = 0.1f;
-
-
-
-    LUZF lucesf[]={
-        //LUZF(153.039,   12,     -91.6788,   153.039,    4.245,  -91.6788),
-        LUZF(125.788,   4.82238,     -39.5693,   153.039,    4.245,  -91.6788),
-        LUZF(103.334,   12,     -87.42,     103.334,    4.245,  -87.42),
-        LUZF(185.485,   12,     -52.2665,   185.485,    4.245,  -52.2665),
-        LUZF(184.759,   12,     -41.7255,   184.759,    4.245,  -41.7255),
-
-        //LUZF(20,   8.5,    -50.5481,   200.178,    4.245,  -50.5481),
-        LUZF(200.178,   8.5,    -50.5481,   200.178,    4.245,  -50.5481),
-        LUZF(221.009,   8.5,    -53.6585,   221.009,    4.245,  -53.6585),
-        LUZF(242.202,   8.5,    -56.456,    242.202,    4.245,  -56.456),
-        LUZF(267.738,   8.5,    -51.9228,   267.738,    4.245,  -51.9228)
-    };
-    uint8_t nluces=8;
-    LUZF aux;
-    unsigned int offset = uniformOffsets[3];
-    unsigned int offset2 = uniformOffsets[4];
-    unsigned int offset3 = uniformOffsets[5];
-    unsigned int offset4 = uniformOffsets[6];
-    for (uint8_t n = 0; n < nluces; n++)
-    {
-        aux=lucesf[n];
-
-        ((float*)(buffer + offset))[0] = aux.pos[0];
-        ((float*)(buffer + offset))[1] = aux.pos[1];
-        ((float*)(buffer + offset))[2] = aux.pos[2];
-        offset += arrayStrides[3];
-
-        ((float*)(buffer + offset2))[0] = aux.posfoco[0];
-        ((float*)(buffer + offset2))[1] = aux.posfoco[1];
-        ((float*)(buffer + offset2))[2] = aux.posfoco[2];
-        offset2 += arrayStrides[4];
-
-        ((float*)(buffer + offset3))[0] = aux.color[0];
-        ((float*)(buffer + offset3))[1] = aux.color[1];
-        ((float*)(buffer + offset3))[2] = aux.color[2];
-        offset3 += arrayStrides[5];
-
-        *((float *)(buffer + offset4)) = aux.intensidad;
-        offset4 += arrayStrides[6];
-    }
-    /*
-    unsigned int offset = uniformOffsets[7];
-    unsigned int offset2 = uniformOffsets[8];
-    unsigned int offset3 = uniformOffsets[9];
-
-    for (int n = 0; n < nluces; n++)
-    {
-        aux=lucesf[n];
-
-        ((float*)(buffer +offset))[0] = aux.pos[0];
-        ((float*)(buffer +offset))[1] = aux.pos[1];
-        ((float*)(buffer +offset))[2] = aux.pos[2];
-        offset += arrayStrides[7];
-
-        ((float*)(buffer +offset2))[0] = aux.posfoco[0];
-        ((float*)(buffer +offset2))[1] = aux.posfoco[1];
-        ((float*)(buffer +offset2))[2] = aux.posfoco[2];
-        offset2 += arrayStrides[8];
-
-        *((float *)(buffer + offset3)) = aux.intensidad;
-        offset3 += arrayStrides[9];
-
-    }
-    */
 
 
     glGenBuffers(1, &ubo);
     glBindBuffer(GL_UNIFORM_BUFFER, ubo);
-    glBufferData(GL_UNIFORM_BUFFER, 8096, buffer, GL_DYNAMIC_DRAW);
+    glBufferData(GL_UNIFORM_BUFFER, tamanyo, buffer, GL_DYNAMIC_DRAW);
     //glBufferData(GL_UNIFORM_BUFFER, sizeof(luces), &luces, GL_DYNAMIC_DRAW);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
@@ -299,7 +269,8 @@ void DeferredShading::DrawQuad(){
     DEFERRED_SHADER->Bind();
 
     glBindBuffer(GL_UNIFORM_BUFFER, ubo);
-    glUniformBlockBinding(DEFERRED_SHADER->getID(), block_index, binding_point_index);
+    glBufferData(GL_UNIFORM_BUFFER, tamanyo, buffer, GL_DYNAMIC_DRAW);
+    //glUniformBlockBinding(DEFERRED_SHADER->getID(), block_index, binding_point_index);
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, gPosition);
--- thehive/src/Omicron/TLuzDirigida.cpp
+++ thehive/src/Omicron/TLuzDirigida.cpp
@@ -1,6 +1,28 @@
 #include "TLuzDirigida.hpp"
 //#include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <ShaderUniformMapping.hpp>
+#include <glm/glm.hpp>
+#include <string>
+
+
+
+
+#define U5    48
+#define U6    176
+#define U7    304
+#define U8    432
+
+#define S5    16
+#define S6    16
+#define S7    16
+#define S8    16
+
+
+
 TLuzDirigida::TLuzDirigida(){}
 TLuzDirigida::~TLuzDirigida(){}
 
@@ -20,5 +42,34 @@ glm::vec3 TLuzDirigida::getDireccion(){
 // Se quedan vacios
 void TLuzDirigida::beginDraw(){
     //std::cout << "Luz Dirigida" << '\n';
+
+    //
+    //unsigned int offsetpos = uniformOffsets[5] +arrayStrides[5]*n;
+    //unsigned int offsetdir = uniformOffsets[6] +arrayStrides[6]*n;
+    //unsigned int offsetcolor = uniformOffsets[7] +arrayStrides[7]*n;
+    //unsigned int offsetintensidad = uniformOffsets[8] +arrayStrides[8]*n;
+
+        int offsetpos =        U5 +S5*N;
+        int offsetdir =        U6 +S6*N;
+        int offsetcolor =      U7 +S7*N;
+        int offsetintensidad = U8 +S8*N;
+
+        glm::vec4 pos = viewMatrix * modelMatrix[3];
+
+        ((float*)(buffer + offsetpos))[0] = pos[0];
+        ((float*)(buffer + offsetpos))[1] = pos[1];
+        ((float*)(buffer + offsetpos))[2] = pos[2];
+
+        ((float*)(buffer + offsetdir))[0] = direccion[0];
+        ((float*)(buffer + offsetdir))[1] = direccion[1];
+        ((float*)(buffer + offsetdir))[2] = direccion[2];
+
+        ((float*)(buffer + offsetcolor))[0] = color.R;
+        ((float*)(buffer + offsetcolor))[1] = color.G;
+        ((float*)(buffer + offsetcolor))[2] = color.B;
+
+        *((float *)(buffer + offsetintensidad)) = intensidad;
+
+
 }
 void TLuzDirigida::endDraw(){}
